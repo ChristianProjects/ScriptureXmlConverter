7e712bb37ea59bd58f00f6c3b6f258ebd5e51c4d
Alexander Ivanov <alehander42@gmail.com>
2021-12-09 02:00:03 +0200

    Work on initial version of loading

--- src/converter.py
+++ src/converter.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
-from typing import List, Dict
+from typing import List, Dict, Optional
 from enum import Enum
+import os
+import xml.etree.ElementTree as ET
 
 class Format(Enum):
     OPENSONG_FORMAT = 'Opensong format'
-    USX = 'usx'
+    USX_FORMAT = 'usx'
 
 
 class Loader:
@@ -32,7 +34,7 @@ class ScriptureBook:
     index: int
     name: str
     chapters: List[ScriptureChapter]
-    
+
     def __init__(self, index: int, name: str):
         self.index = index
         self.name = name
@@ -51,19 +53,96 @@ class UsxFormatLoader(Loader):
     def __init__(self, path: str):
         self.path = path
     
-    def load(self):
+    def load(self) -> None:
         '''
         loads `self.path` and converts it to a tree
         '''
-        # open dir
-        # traverse dir
+        # open and traverse dir
         # for each file
         #   add a book
         #   traverse xml there and add chapters and for each chapter find just verses and text and add those
         #   this is it
-        pass
 
-    def tree(self):
+        self._tree = ScriptureTree()
+
+        for _, _, files in os.walk(self.path):
+            for i, file in enumerate(sorted(files)):
+                if file[0] != '0':
+                    break
+                book = self.load_book(i + 1, file)
+
+                self._tree.books.append(book)
+                break # TODO remove
+        print(self._tree.books[0].chapters[0].verses[0].__dict__)
+
+    def load_book(self, index: int, file: str) -> ScriptureBook:
+        with open(os.path.join(self.path, file), 'r') as book_file:
+            raw = book_file.read()
+
+        xml = ET.fromstring(raw)
+
+        # find title: first <para style="h">[title]</para>
+        title_element = xml.find('para/[@style="h"]')
+        if title_element is None:
+            raise ValueError('expected title element')
+        title = title_element.text
+        book = ScriptureBook(index, title)
+        print(book.__dict__)
+
+        chapter_index = -1
+        verses = []
+        in_chapter = False
+        verse_index = -1
+        verse_text = ''
+        for item in xml:
+            if item.tag == 'chapter':
+                if chapter_index != -1:
+                    chapter_value = ScriptureChapter(chapter_index)
+                    chapter_value.verses = verses
+                    book.chapters.append(chapter_value)
+                chapter_index = int(item.attrib['number'])
+                in_chapter = True
+                verses = []
+            elif in_chapter and item.tag == 'para' and item.attrib['style'] == 'p':
+                # print(item.text)
+                # for raw_text in item.itertext():
+                    # text = raw_text.strip()
+                    # if len(text) > 0:
+                        # print(text)
+                for chapter_item in item:
+                    if chapter_item.tag == 'verse':
+                        if verse_index != -1:
+                            verse_text = verse_text.strip()
+                            verse_value = ScriptureVerse(verse_index, verse_text)
+                            verses.append(verse_value)
+                        verse_index = int(chapter_item.attrib['number'])
+                        verse_text = ''
+                    elif chapter_item.tag == None:
+                        print(chapter_item)
+                    elif chapter_item.tag == 'note' or chapter_item.tag == 'char':
+                        if chapter_item.tag == 'char' and chapter_item.attrib['style'] in {'k', 'nd'}:
+                            if chapter_item.text is not None:
+                                verse_text + chapter_item.text + ' '
+                        elif chapter_item.tag == 'char' and chapter_item.attrib['style'] == 'add':
+                            # TODO: are those actually part of the Scripture?                            
+                            if chapter_item.text is not None:
+                                verse_text += '*' + chapter_item.text + '*'
+                        if chapter_item.tail is not None and len(chapter_item) > 0:
+                            verse_text += chapter_item.tail + ' '
+
+
+                        # print(list(chapter_item.itertext()))
+        if verse_index != -1:
+            verse_text = verse_text.strip()
+            verse_value = ScriptureVerse(verse_index, verse_text)
+            verses.append(verse_value)
+        if chapter_index != -1:
+            chapter_value = ScriptureChapter(chapter_index)
+            chapter_value.verses = verses
+            book.chapters.append(chapter_value)
+        return book
+
+    def tree(self) -> ScriptureTree:
         return self._tree
 
 class Generator:
@@ -85,7 +164,7 @@ def run(path: str, output_path: str, from_format: Format, to: Format):
     converts a file in `path` from `from_format` to `to` and saves it in `output_path`.
     currently only an usx -> Opensong convert is supported
     '''
-    if from_format == Format.USX and to == Format.OPENSONG_FORMAT:
+    if from_format == Format.USX_FORMAT and to == Format.OPENSONG_FORMAT:
         loader = UsxFormatLoader(path)
         loader.load()
         generator = OpensongFormatGenerator(loader.tree())
@@ -95,8 +174,8 @@ def run(path: str, output_path: str, from_format: Format, to: Format):
 if __name__ == '__main__':
     # TODO load args
     # converter.py <path> <output_path> <from> <to>
-    path = 'tests/from'
-    output_path = 'tests/to.xml'
+    path = 'from' #'tests/from'
+    output_path = 'to.xml' #'tests/to.xml'
     from_format = Format.USX_FORMAT
     to = Format.OPENSONG_FORMAT
     run(path, output_path, from_format, to)
